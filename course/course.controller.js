0b08dec5106dfb8c21fe78d0338411a9af080447
neupanesaugat <saugatneupane50@gmail.com>
2024-07-17 18:00:07 +0545

    Narrowway api using express js

--- course/course.controller.js
+++ course/course.controller.js
@@ -12,6 +12,8 @@ router.post(
   "/add",
 
   async (req, res, next) => {
+    // validate token
+
     validateReqBody(validateCourseSchema);
     //? extract token from req.headers
     const authorization = req.headers.authorization;
@@ -62,6 +64,75 @@ router.post(
 
     //send res
     return res.status(201).send({ message: "Course is added successfully" });
+  }
+);
+
+//* get course list
+router.get(
+  "/list",
+  //validate token
+  async (req, res, next) => {
+    validateReqBody(validateCourseSchema);
+    //? extract token from req.headers
+    const authorization = req.headers.authorization;
+
+    const splittedToken = authorization?.split(" "); //? splitting Bearer and token , ? => checking whether or not there is Authorization
+    const token = splittedToken?.length === 2 ? splittedToken[1] : null; //? splittedToken has array of length 2, so token will be available only of there is Authorization and token, ? in middle refers to ternary operator
+
+    //? if not token, throw error
+    if (!token) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    //? verify token, (if verification fails, throw error)
+    let payload;
+
+    try {
+      const sign = "klsdfjlksjlk";
+      payload = jwt.verify(token, sign);
+    } catch (error) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    console.log(payload);
+
+    //? find admin using payload
+    const admin = await Admin.findOne({ email: payload.email });
+
+    //? if not admin, throw error
+    if (!admin) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    //? extracting id of admin from the token
+    req.loggedInUserId = admin._id; // adding new field in request with admin id
+
+    next();
+  },
+  async (req, res) => {
+    const courses = await Course.aggregate([
+      {
+        $match: {},
+      },
+      {
+        $lookup: {
+          from: "admins",
+          localField: "addedBy",
+          foreignField: "_id",
+          as: "adminDetails",
+        },
+      },
+
+      {
+        $project: {
+          name: 1,
+          price: 1,
+          duration: 1,
+          adminEmail: { $first: "$adminDetails.email" },
+        },
+      },
+    ]);
+    return res.status(200).send({ message: "Success", courseList: courses });
   }
 );
 
